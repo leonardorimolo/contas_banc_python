c9f99ca156a5caeddcdd398f1d36d08527a93cb0
Lucas Tabelli Berr <68754446+LucasBerr@users.noreply.github.com>
2023-10-03 19:03:17 -0300

    Adicionando correntista
    Adicionando a classe do correntista

---
+++ main/correntista.py
@@ -0,0 +1,22 @@
+class Correntista:
+    def __init__(self, nome, email):
+        self._nome = nome
+        self._email = email
+
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome
+
+    @property
+    def email(self):
+        return self._email
+    
+    @email.setter
+    def email(self, novo_email):
+        self._email = novo_email
+
+    
